b8ea314a97d75b98fb9c9b173875f18547165017
geeks@abhimanyu <joinabhimanyu@gmail.com>
2024-11-08 10:49:12 +0530

    added login screen animation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,20 +1,39 @@
-import {DarkTheme, DefaultTheme, ThemeProvider} from '@react-navigation/native';
-import {useFonts} from 'expo-font';
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
 
 import * as SplashScreen from 'expo-splash-screen';
-import {useEffect} from 'react';
+import { useEffect } from 'react';
 import 'react-native-reanimated';
-import {useColorScheme} from '@/hooks/useColorScheme';
-import {Stack} from "expo-router";
-import {SafeAreaProvider} from "react-native-safe-area-context";
-import {SafeAreaView} from "react-native";
-import {Provider} from "react-redux";
+import { useColorScheme } from '@/hooks/useColorScheme';
+import { Stack } from "expo-router";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { SafeAreaView } from "react-native";
+import { Provider } from "react-redux";
 import store from "@/redux/store";
 
 export default function RootLayout() {
     const colorScheme = useColorScheme();
     const [loaded] = useFonts({
-        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+        SpaceMono: require('@/assets/fonts/SpaceMono-Regular.ttf'),
+        RobotoMono: require('@/assets/fonts/RobotoMono-Regular.ttf'),
+
+        HelveticaNeueUltraLightv130: require('@/assets/fonts/HelveticaNeueUltraLightv130.ttf'),
+        HelveticaLight: require('@/assets/fonts/HelveticaLight.ttf'),
+        HelveticaNeueHv: require('@/assets/fonts/HelveticaNeueHv.ttf'),
+        HelveticaNeueIt: require('@/assets/fonts/HelveticaNeueIt.ttf'),
+
+        NunitoSansCyrillic900Normal: require('@/assets/fonts/nunito-sans-cyrillic-900-normal.ttf'),
+        NunitoSansCyrillic600Normal: require('@/assets/fonts/nunito-sans-cyrillic-600-normal.ttf'),
+        NunitoSansCyrillic700Normal: require('@/assets/fonts/nunito-sans-cyrillic-700-normal.ttf'),
+        NunitoSansCyrillic800Normal: require('@/assets/fonts/nunito-sans-cyrillic-800-normal.ttf'),
+        InterCyrillic900Normal: require('@/assets/fonts/inter-cyrillic-900-normal.ttf'),
+        InterCyrillic600Normal: require('@/assets/fonts/inter-cyrillic-600-normal.ttf'),
+        InterCyrillic700Normal: require('@/assets/fonts/inter-cyrillic-700-normal.ttf'),
+        InterCyrillic800Normal: require('@/assets/fonts/inter-cyrillic-800-normal.ttf'),
+        JetbrainsMonoCyrillic800Normal: require('@/assets/fonts/jetbrains-mono-cyrillic-800-normal.ttf'),
+        JetbrainsMonoCyrillic600Normal: require('@/assets/fonts/jetbrains-mono-cyrillic-600-normal.ttf'),
+        JetbrainsMonoCyrillic700Normal: require('@/assets/fonts/jetbrains-mono-cyrillic-700-normal.ttf'),
+        MaterialIconsRoundLatin400Normal: require('@/assets/fonts/material-icons-round-latin-400-normal.ttf'),
     });
 
     useEffect(() => {
@@ -30,9 +49,10 @@ export default function RootLayout() {
         <Provider store={store}>
             <SafeAreaProvider>
                 <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-                    <Stack>
-                        <Stack.Screen name="(drawer)" options={{headerShown: false}}/>
-                        <Stack.Screen name='+not-found'/>
+                    <Stack initialRouteName='index'>
+                        <Stack.Screen name="index" options={{ headerShown: false }} />
+                        <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
+                        <Stack.Screen name='+not-found' />
                     </Stack>
                 </ThemeProvider>
             </SafeAreaProvider>
--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,201 @@
-import React from 'react'
-import {Redirect} from "expo-router";
+import { View, Text, StyleSheet, TextInput, TouchableOpacity, Dimensions } from 'react-native'
+import React, { useEffect, useRef, useState } from 'react'
+import baseStyles from '@/styles/baseStyles'
+import { useColorScheme } from '@/hooks/useColorScheme'
+import { Colors } from '@/constants/Colors'
+import { Ionicons } from '@expo/vector-icons'
+import { Link } from 'expo-router'
+import Animated, { Easing, useAnimatedStyle, useSharedValue, withRepeat, withSequence, withSpring, withTiming } from 'react-native-reanimated'
 
-export default function Index() {
+const Home = () => {
+    const colorScheme = useColorScheme();
+    const { width } = Dimensions.get('screen');
+    const fontsize = useSharedValue(0);
+    const marginTop = useSharedValue(0);
+
+    useEffect(() => {
+
+        marginTop.value = withRepeat(
+            withSequence(
+                withTiming(
+                    20,
+                    { duration: 300, easing: Easing.linear }
+                ),
+                withTiming(
+                    40,
+                    { duration: 200, easing: Easing.linear }
+                ),
+                withTiming(
+                    60,
+                    { duration: 150, easing: Easing.linear }
+                ),
+                withTiming(
+                    80,
+                    { duration: 100, easing: Easing.linear }
+                ),
+            ),
+            1
+        );
+
+        fontsize.value = withRepeat(
+            withSequence(
+                withTiming(
+                    16,
+                    { duration: 150, easing: Easing.out(Easing.exp) }
+                ),
+                withTiming(
+                    26,
+                    { duration: 150, easing: Easing.out(Easing.exp) }
+                ),
+                withTiming(
+                    45,
+                    { duration: 100, easing: Easing.out(Easing.exp) }
+                ),
+            ),
+            1
+        );
+
+    }, []);
+
+    const animatedStyle = useAnimatedStyle(() => ({
+        fontSize: fontsize.value
+    }))
+
+    const containerAnimatedStyle = useAnimatedStyle(() => ({
+        marginTop: marginTop.value
+    }))
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: '#F5F5F5'
+        },
+        userNameContainer: {
+            marginBottom: 20
+        },
+        passwordContainer: {
+            marginBottom: 10
+        },
+        field: {
+            borderWidth: 1,
+            borderColor: '#ccc',
+            padding: 10,
+            borderRadius: 10,
+            backgroundColor: '#fff',
+            fontSize: 16,
+            height: 50,
+            width: width * 0.8,
+        },
+        logoContainer: {
+            marginBottom: 40,
+            // marginTop: 80
+        },
+        fontText: {
+            // fontSize: 40,
+            fontFamily: 'HelveticaLight',
+            fontWeight: 600,
+            color: 'black',
+            textShadowColor: 'gray',
+            textShadowOffset: { width: 2, height: 2 },
+            textShadowRadius: 5
+        }
+    });
+
+    const [showCloseIconUsername, setShowCloseIconUsername] = useState(false);
+    const [showCloseIconPassword, setShowCloseIconPassword] = useState(false);
+
+    const onFocusUserNameHandler = () => {
+        setShowCloseIconUsername(true)
+    }
+
+    const onBlurUserNameHandler = () => {
+        setShowCloseIconUsername(false)
+    }
+
+    const onFocusPasswordHandler = () => {
+        setShowCloseIconPassword(true)
+    }
+
+    const onBlurPasswordHandler = () => {
+        setShowCloseIconPassword(false)
+    }
+
+    const clearUserNameHandler = () => false
+
+    const clearPasswordHandler = () => false
+
+    const showPasswordHandler = () => false
+
+    const onLoginPressHandler = () => {
+        // Handle login logic here
+    }
+
     return (
-        <Redirect href="/(drawer)/products"/>
+        <View style={styles.container}>
+            <Animated.View style={[containerAnimatedStyle, styles.logoContainer]}>
+                <Animated.Text style={[animatedStyle, styles.fontText]}>Instagramicons</Animated.Text>
+                {/* <Text style={styles.fontText}>Instagramicons</Text> */}
+            </Animated.View>
+            <View style={{ justifyContent: 'flex-start', alignItems: 'flex-start' }}>
+
+                <View style={styles.userNameContainer}>
+                    <TextInput
+                        placeholder='Enter user name'
+                        onFocus={onFocusUserNameHandler}
+                        onBlur={onBlurUserNameHandler}
+                        style={[ styles.field]} />
+
+                    <View style={{ position: 'absolute', right: 10, top: 15 }}>
+
+                        {showCloseIconUsername ? (
+                            <TouchableOpacity onPress={clearUserNameHandler}>
+                                <Ionicons name='close-circle' size={20} color="gray" />
+                            </TouchableOpacity>
+                        ) : null}
+
+                    </View>
+                </View>
+
+                <View style={styles.passwordContainer}>
+                    <TextInput
+                        placeholder='Enter password'
+                        onFocus={onFocusPasswordHandler}
+                        onBlur={onBlurPasswordHandler}
+                        secureTextEntry={true} style={[ styles.field]} />
+
+                    <View style={{ position: 'absolute', right: 10, top: 15 }}>
+
+                        {showCloseIconPassword ? (
+                            <TouchableOpacity onPress={clearPasswordHandler}>
+                                <Ionicons name='close-circle' size={20} color="gray" />
+                            </TouchableOpacity>
+                        ) : (
+                            <TouchableOpacity onPress={showPasswordHandler}>
+                                <Ionicons name='eye' size={20} color="gray" />
+                            </TouchableOpacity>
+                        )}
+
+                    </View>
+                </View>
+
+                <Link href=".." style={{ alignSelf: 'flex-end', marginBottom: 30 }} onPress={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                }}>
+                    <Text style={{ color: 'gray', fontWeight: 'bold', marginTop: 10 }}>Forgot Password?</Text>
+                </Link>
+
+            </View>
+
+            <TouchableOpacity onPress={onLoginPressHandler} style={[baseStyles.primaryButton, {
+                backgroundColor: Colors[colorScheme ?? 'light'].primaryButtonColor,
+            }]}>
+                <Text style={{ color: 'white' }}>Login</Text>
+            </TouchableOpacity>
+        </View>
     )
 }
+
+export default Home
